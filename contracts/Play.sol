9c247050be5824339e65be9f358a2c961ab8835a
Ermias <ermiasnigat17@gmail.com>
2024-12-19 15:20:13 +0300

    added NatSpec doc to the play ground

--- contracts/Play.sol
+++ contracts/Play.sol
@@ -1,19 +1,34 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HelloMessage is Ownable   {
-    string private message = "Hello world";  // message stored on-chain
+/// @title HelloMessage Contract
+/// @notice This contract allows the owner to store and update a message on-chain.
+/// @dev Inherits `Ownable` from OpenZeppelin to restrict certain functions to the contract owner.
+contract HelloMessage is Ownable {
+    /// @notice The message stored on-chain.
+    string public message = "Hell0";
 
+    /// @dev Emitted when the message is updated.
+    /// @param newMessage The updated message.
     event MessageUpdated(string newMessage);
 
-    function updateMessage(string memory _msg)  public onlyOwner  {
+    /// @notice Updates the stored message.
+    /// @dev Only callable by the owner of the contract.
+    /// @param _msg The new message to store.
+    function updateMessage(string memory _msg) public onlyOwner {
+        require(bytes(_msg).length > 0, "Message cannot be empty!");
+        //require(keccak256(abi.encodePacked(message)) != keccak256(abi.encodePacked(_msg)), "There is nothing changed!");
+        require(keccak256(bytes(message)) != keccak256(bytes(_msg)), "There is nothing changed!"); // more optimized as solidity is now supprting string comarition natively
+
         message = _msg;
         emit MessageUpdated(_msg);
     }
 
-    function retrieveMessage() public view returns (string memory) 
-    {
+    /// @notice Retrieves the current stored message.
+    /// @return The current message stored on-chain.
+    function getMessage() public view returns (string memory) {
         return message;
     }
 }
